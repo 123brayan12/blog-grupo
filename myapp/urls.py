76284aca2903c82463bc33d7311447b7dd268aea
Brayan Tamiche <brayan.tamiche.g.dbb@gmail.com>
2024-02-24 09:49:28 -0400

    modelos

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import BlogCreateView
 
 urlpatterns = [
+    
     path("post/new",BlogCreateView.as_view(),name="post_new"),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Post
 
-
-
-
 class BlogCreateView(CreateView):
     model=Post
     template_name='Createpost.html'

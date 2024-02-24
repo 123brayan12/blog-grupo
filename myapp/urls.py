ad9643baf20e2075beb442a209eb300c62d8044f
wil1an <tamichegwilliam@gmail.com>
2024-02-24 11:47:57 -0400

    delete

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from .views import BlogUpdateView
 from .views import BlogDeleteView
 
 urlpatterns = [
-    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit")
-    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete")
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
 ]
 

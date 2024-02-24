81d487581c82010ba8e119f35e0438ef0ce5509e
wil1an <tamichegwilliam@gmail.com>
2024-02-24 11:45:56 -0400

    delete

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,10 @@
 from .views import hello_world
 from django.urls import path
+from .views import BlogUpdateView
+from .views import BlogDeleteView
 
 urlpatterns = [
-    path("", hello_world, name='helloworld'),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit")
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete")
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,14 @@
+from django.views.generic.edit import CreateView, UpdateView 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
+from django.urls import reverse_lazy 
 
-def hello_world (request):
-    return HttpResponse("Hello world")
 
+class BlogUpdateView(UpdateView): 
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+class BlogDeleteView(DeleteView): 
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")

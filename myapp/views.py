3be2b30755011d9e96bed4ca5d93b230b13e45b1
SergioMC <you@example.com>
2024-03-02 11:09:52 -0400

    Adding missing files 3

--- myapp/views.py
+++ myapp/views.py
@@ -20,8 +20,3 @@ class BlogCreateView(CreateView):
     model=Post
     template_name='Createpost.html'
     fields=["title","author","body"]
-
-
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from .models import Post

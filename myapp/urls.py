71f4b58ad318919e59ede1d45ab2f57530ea397c
SergioMC <you@example.com>
2024-03-02 10:57:47 -0400

    Adding missing files 1

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
-from .views import BlogDetailView, BlogListView
+from .views import BlogDetailView, BlogListView, BlogCreateView
 from django.urls import path
 
 urlpatterns = [
-    path("", hello_world, name='helloworld'),
+    path("", BlogListView.as_view(), name='MainSite'),
+    path("post/new",BlogCreateView.as_view(),name="post_new"),
+    path("post/<int:pk>/", BlogDetailView.as_view(),name="post_detail"),
 ]
 

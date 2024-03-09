f06d9b58522ad87656aba51e738a56fc4399399e
wil1an <tamichegwilliam@gmail.com>
2024-03-09 10:09:44 -0400

    pul

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,14 @@
 from .views import BlogDetailView, BlogListView
 from django.urls import path
+from .views import BlogUpdateView
+from .views import BlogDeleteView
+from .views import BlogCreateView
 
 urlpatterns = [
-    path("", hello_world, name='helloworld'),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
+    path("", BlogListView.as_view(), name='MainSite'),
+    path("post/new",BlogCreateView.as_view(),name="post_new"),
+    path("post/<int:pk>/", BlogDetailView.as_view(),name="post_detail"),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,35 @@
+from django.views.generic.edit import CreateView, UpdateView 
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
+from django.urls import reverse_lazy 
+
+
+from django.views.generic.edit import CreateView
+from .models import Post
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
 
-def hello_world (request):
-    return HttpResponse("Hello world")
 
+class BlogUpdateView(UpdateView): 
+    model = Post
+    template_name = "update.html"
+    fields = ["title", "body"]
+class BlogDeleteView(DeleteView): 
+    model = Post
+    template_name = "delete.html"
+    success_url = reverse_lazy("MainSite")
+ 
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "BlogDetail.html" 
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='Createpost.html'
+    fields=["title","author","body"]
